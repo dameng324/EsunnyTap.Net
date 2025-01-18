954c013770b40a77bd88e6d54f7dcc28f5df3b21
Dameng <313880747@qq.com>
2025-01-18 22:54:27 +0800

    add nuke build and github action

---
+++ build/Build.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.PathConstruction;
+
+[GitHubActions(
+    "publish",
+    GitHubActionsImage.WindowsLatest,
+    On = new[] { GitHubActionsTrigger.Push },
+    ImportSecrets = new[] { nameof(NuGetApiKey) },
+    InvokedTargets = new[] { nameof(Compile) }
+)]
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main() => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution]
+    readonly Solution Solution;
+    Project Project => Solution.GetProject("EsunnyTap.Net");
+
+    [Parameter]
+    [Secret]
+    readonly string NuGetApiKey;
+    GitHubActions GitHubActions => GitHubActions.Instance;
+    Target Clean =>
+        _ =>
+            _.Before(Restore)
+                .Executes(() =>
+                {
+                    DotNetTasks.DotNetClean(s => s.SetProject(Project));
+                });
+
+    Target Restore =>
+        _ =>
+            _.Executes(() =>
+            {
+                DotNetTasks.DotNetRestore(s => s.SetProjectFile(Project));
+            });
+
+    Target Compile =>
+        _ =>
+            _.DependsOn(Restore)
+                .Executes(() =>
+                {
+                    DotNetTasks.DotNetBuild(s => s.SetProjectFile(Project).SetConfiguration(Configuration));
+                    if (IsServerBuild && GitHubActions.Instance.RefType.Equals("tag", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var tag = GitHubActions.Instance.Ref;
+                        var outDir = Project.Directory / "bin" / Configuration;
+                        outDir.CreateOrCleanDirectory();
+                        DotNetTasks.DotNetPack(s =>
+                            s.SetProject(Project).SetConfiguration(Configuration).SetOutputDirectory(outDir).SetProperty("Version", tag)
+                        );
+
+                        var nupkg = outDir.GlobFiles("*.nupkg").First();
+                        DotNetTasks.DotNetNuGetPush(s =>
+                            s.SetTargetPath(nupkg).SetSource("https://api.nuget.org/v3/index.json").SetApiKey(NuGetApiKey)
+                        );
+                    }
+                });
+}
